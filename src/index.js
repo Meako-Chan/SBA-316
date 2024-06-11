61266fb2b934a07f91cdf619daf779fdd4b2953a
Michael Chan <michael.w.chan01@gmail.com>
2024-06-11 01:55:49 -0400

    Quiz formatting

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+
+let quiz = [
+    {
+        number: 1,
+        question: "What color does a Saiyan's hair become when turning into a Super Saiyan?",
+        choice1: "red",
+        choice2: "blue",
+        choice3: "green",
+        choice4: "yellow",
+        answer: "yellow",
+    }
+    {
+        number: 2,
+        question: "Who was the first character to achieve Super Saiyan 2?",
+        choice1: "Goku",
+        choice2: "Gohan",
+        choice3: "Vegeta",
+        choice4: "Trunks",
+        answer: "Gohan",
+    }
+    {
+        number: 3,
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    }
+]
+
+question = document.getElementById("question");
+console.log(question)
